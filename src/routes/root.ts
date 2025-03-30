ae45485720829132d8ebc5bec6baf0b0d7a325ce
Awais Shahid <i212764@nu.edu.pk>
2025-03-30 19:25:28 +0500

    return server env

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,16 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
-
-export default root
+  fastify.get("/", async function (request, reply) {
+    return {
+      "Server Env": {
+        BLOCKCHAIN_RPC_PROVIDER: process.env.BLOCKCHAIN_RPC_PROVIDER,
+        BLOCKCHAIN_CHAIN_ID: process.env.BLOCKCHAIN_CHAIN_ID,
+        BLOCKCHAIN_DID_REGISTRY_ADDR: process.env.BLOCKCHAIN_DID_REGISTRY_ADDR,
+        VERAMO_SECRET_KEY: process.env.VERAMO_SECRET_KEY,
+      },
+      "Routes available": "Not implemented yet",
+    };
+  });
+};
+export default root;
